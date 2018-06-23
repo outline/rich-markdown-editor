64e2b991217de7be533f66bdf39f03f8c7a9a169
Tom Moor <tom.moor@gmail.com>
2018-06-22 22:58:45 -0700

    Update example to include read only toggle

--- example/src/index.js
+++ example/src/index.js
@@ -1,5 +1,5 @@
 // @flow
-import React from "react";
+import * as React from "react";
 import ReactDOM from "react-dom";
 import Editor from "../../src";
 
@@ -11,30 +11,47 @@ const exampleText = `
 This is example content. It is persisted between reloads in localStorage.
 `;
 const defaultValue = savedText || exampleText;
+class Example extends React.Component<*, { readOnly: boolean }> {
+  state = {
+    readOnly: false,
+  };
+
+  handleToggle = () => {
+    this.setState({ readOnly: !this.state.readOnly });
+  };
+
+  render() {
+    return (
+      <div style={{ marginTop: "60px" }}>
+        <button type="button" onClick={this.handleToggle}>
+          {this.state.readOnly ? "Editable" : "Read Only"}
+        </button>
+        <Editor
+          readOnly={this.state.readOnly}
+          defaultValue={defaultValue}
+          onSave={() => console.log("Save triggered")}
+          onCancel={() => console.log("Cancel triggered")}
+          onChange={text => localStorage.setItem("saved", text)}
+          onClickLink={href => console.log("Clicked link: ", href)}
+          onSearchLink={async term => {
+            console.log("Searched link: ", term);
+            return [
+              {
+                title: term,
+                url: "localhost",
+              },
+            ];
+          }}
+          uploadImage={async file => {
+            console.log("File upload triggered: ", file);
+            return "";
+          }}
+        />
+      </div>
+    );
+  }
+}
 
 if (element) {
-  ReactDOM.render(
-    <Editor
-      style={{ marginTop: "60px" }}
-      defaultValue={defaultValue}
-      onSave={() => console.log("Save triggered")}
-      onCancel={() => console.log("Cancel triggered")}
-      onChange={text => localStorage.setItem("saved", text)}
-      onClickLink={href => console.log("Clicked link: ", href)}
-      onSearchLink={async term => {
-        console.log("Searched link: ", term);
-        return [
-          {
-            title: term,
-            url: "localhost",
-          },
-        ];
-      }}
-      uploadImage={async file => {
-        console.log("File upload triggered: ", file);
-        return "";
-      }}
-    />,
-    element
-  );
+  ReactDOM.render(<Example />, element);
 }
