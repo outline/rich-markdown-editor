11a3e989f006a5eb8064541fe04b927bfd60b6a0
Jori Lallo <jtlallo@gmail.com>
2018-07-13 12:12:10 -0700

    Slug without unicode

--- src/lib/headingToSlug.js
+++ src/lib/headingToSlug.js
@@ -1,15 +1,15 @@
 // @flow
 import { escape } from "lodash";
 import { Document, Block, Node } from "slate";
-import slug from "slug";
+import slugify from "slugify";
 
 // finds the index of this heading in the document compared to other headings
 // with the same slugified text
 function indexOfType(document, heading) {
-  const slugified = escape(slug(heading.text));
+  const slugified = escape(slugify(heading.text));
   const headings = document.nodes.filter((node: Block) => {
     if (!node.text) return false;
-    return node.type.match(/^heading/) && slugified === escape(slug(node.text));
+    return node.type.match(/^heading/) && slugified === escape(slugify(node.text));
   });
 
   return headings.indexOf(heading);
@@ -18,7 +18,7 @@ function indexOfType(document, heading) {
 // calculates a unique slug for this heading based on it's text and position
 // in the document that is as stable as possible
 export default function headingToSlug(document: Document, node: Node) {
-  const slugified = escape(slug(node.text));
+  const slugified = escape(slugify(node.text));
   const index = indexOfType(document, node);
   if (index === 0) return slugified;
   return `${slugified}-${index}`;
