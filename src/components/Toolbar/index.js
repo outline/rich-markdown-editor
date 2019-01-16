5cb8b263665e0a002070f548813a633ae834336a
Tom Moor <tom.moor@gmail.com>
2019-01-15 19:25:50 -0800

    Make toolbar more responsive

--- src/components/Toolbar/index.js
+++ src/components/Toolbar/index.js
@@ -203,7 +203,7 @@ const Menu = styled.div`
   transform: scale(0.95);
   transition: opacity 150ms cubic-bezier(0.175, 0.885, 0.32, 1.275),
     transform 150ms cubic-bezier(0.175, 0.885, 0.32, 1.275);
-  transition-delay: 250ms;
+  transition-delay: 150ms;
   line-height: 0;
   height: 40px;
   box-sizing: border-box;
