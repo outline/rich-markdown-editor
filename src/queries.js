14cb327e9f890fb313d1762c07e035b1eadbc3ed
Tom Moor <tom.moor@gmail.com>
2019-03-06 23:37:51 -0800

    :cleanup:

--- src/queries.js
+++ src/queries.js
@@ -1,29 +1,10 @@
 // @flow
 import { Editor, Value } from "slate";
-import { List } from "immutable";
 
 const queries = {
   isLinkActive(editor: Editor, value: Value) {
     const { inlines } = value;
     return inlines.some(i => i.type === "link");
-  },
-
-  getHighestSelectedBlocks(editor: Editor) {
-    const { value } = editor;
-    const range = value.selection;
-    const { document } = value;
-
-    const startBlock = document.getClosestBlock(range.start.key);
-    const endBlock = document.getClosestBlock(range.end.key);
-
-    if (startBlock === endBlock) {
-      return List([startBlock]);
-    }
-    const ancestor = document.getCommonAncestor(startBlock.key, endBlock.key);
-    const startPath = ancestor.getPath(startBlock.key);
-    const endPath = ancestor.getPath(endBlock.key);
-
-    return ancestor.nodes.slice(startPath[0], endPath[0] + 1);
   },
 };
 
