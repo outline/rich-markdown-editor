c36e17dd9251a876cef34c0ce49d87dc53f2f603
Tom Moor <tom.moor@gmail.com>
2019-11-09 12:42:44 -0800

    fix: Improve behavior adding pound symbol before already written text

--- src/plugins/Hashtags.js
+++ src/plugins/Hashtags.js
@@ -5,23 +5,39 @@ import InstantReplace from "slate-instant-replace";
 const regex = hashtagRegex();
 
 function Hashtags() {
-  return InstantReplace((editor, lastWord) => {
-    if (lastWord) {
-      const match = lastWord.match(regex);
+  return InstantReplace((editor, lastWord: string) => {
+    if (!lastWord) return;
+    const match = lastWord.match(regex);
+
+    // captures when "#" added before existing characters
+    if (lastWord === "#") {
+      const { startText } = editor.value;
+      const text = startText.text;
+      const match = regex.exec(text);
 
       if (match) {
-        const diff = lastWord.length - match[0].length;
-
         editor
           .unwrapInline({ type: "hashtag" })
-          .moveStartBackward(lastWord.length)
-          .moveEndBackward(diff)
+          .moveStartTo(startText.key, match.index)
+          .moveEndTo(startText.key, match.index + match[0].length)
           .wrapInline({ type: "hashtag" })
-          .moveForward(diff)
-          .moveToEnd();
-      } else {
-        editor.unwrapInline({ type: "hashtag" });
+          .moveToStart()
+          .moveForward(1);
       }
+
+      // captures when characters added after a "#"
+    } else if (match) {
+      const diff = lastWord.length - match[0].length;
+
+      editor
+        .unwrapInline({ type: "hashtag" })
+        .moveStartBackward(lastWord.length)
+        .moveEndBackward(diff)
+        .wrapInline({ type: "hashtag" })
+        .moveForward(diff)
+        .moveToEnd();
+    } else {
+      editor.unwrapInline({ type: "hashtag" });
     }
   });
 }
