aeaa79b573e5deedab71426a107dbe9084f3dc4e
Tom Moor <tom.moor@gmail.com>
2020-01-19 20:51:38 -0800

    cleanup

--- src/slate-oldies/EditList.js
+++
@@ -1,8 +0,0 @@
-// @flow
-import EditList from "@tommoor/slate-edit-list";
-
-export default EditList({
-  types: ["ordered-list", "bulleted-list", "todo-list"],
-  typeItem: "list-item",
-  typeDefault: "paragraph",
-});
