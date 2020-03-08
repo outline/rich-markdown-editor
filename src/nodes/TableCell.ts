cd3b5fc38b57e33b51b61dfa91adc8cbebb4129a
Tom Moor <tom.moor@gmail.com>
2020-03-07 18:09:38 -0800

    cleanup

--- src/nodes/TableCell.ts
+++ src/nodes/TableCell.ts
@@ -13,7 +13,7 @@ export default class TableCell extends Node {
   toMarkdown(state, node) {
     state.renderInline(node);
     state.closed = false;
-    state.out = `${state.out.replace(/\n?\n$/, "")} | `;
+    state.out = `${state.out.replace(/\\?\n?\n$/, "")} | `;
   }
 
   parseMarkdown() {
