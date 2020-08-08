417825fca7976bdbb5240c8a342a08368d084e59
Dominic Zijlstra <dominiczijlstra@Dominics-Air.lan>
2020-08-08 21:50:21 +0200

    Remove opacity

--- src/index.tsx
+++ src/index.tsx
@@ -795,7 +795,6 @@ const Wrapper = styled.div<{
   border-radius: 4px;
   box-shadow: rgba(0, 0, 0, 0.05) 0px 0px 0px 1px,
     rgba(0, 0, 0, 0.08) 0px 4px 8px, rgba(0, 0, 0, 0.08) 0px 2px 4px;
-  opacity: 0;
   transform: scale(0.95);
   transition: opacity 150ms cubic-bezier(0.175, 0.885, 0.32, 1.275),
     transform 150ms cubic-bezier(0.175, 0.885, 0.32, 1.275);
@@ -820,7 +819,6 @@ const Wrapper = styled.div<{
     `
     transform: translateY(${isAbove ? "6px" : "-6px"}) scale(1);
     pointer-events: all;
-    opacity: 1;
   `};
 
   @media print {
