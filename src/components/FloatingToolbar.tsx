6d14f89a13332d62a6450b31662292e2f1213a3c
Tom Moor <tom.moor@gmail.com>
2020-09-06 14:02:50 -0700

    fix: Refactoring floating toolbar to hooks and ResizeObserver

--- src/components/FloatingToolbar.tsx
+++ src/components/FloatingToolbar.tsx
@@ -1,6 +1,6 @@
+import ResizeObserver from "resize-observer-polyfill";
 import * as React from "react";
 import { Portal } from "react-portal";
-import { isEqual } from "lodash";
 import { EditorView } from "prosemirror-view";
 import styled from "styled-components";
 
@@ -13,130 +13,144 @@ type Props = {
   forwardedRef?: React.RefObject<HTMLDivElement> | null;
 };
 
-class FloatingToolbar extends React.Component<Props> {
-  menuRef = this.props.forwardedRef || React.createRef<HTMLDivElement>();
+const defaultPosition = {
+  left: -1000,
+  top: 0,
+  offset: 0,
+  visible: false,
+};
 
-  state = {
-    left: -1000,
-    top: 0,
-    offset: 0,
-    visible: false,
-    isSelectingText: false,
-  };
+const useComponentSize = comRef => {
+  const [size, setSize] = React.useState({
+    width: 0,
+    height: 0,
+  });
 
-  componentDidMount() {
-    window.addEventListener("mousedown", this.handleMouseDown);
-    window.addEventListener("mouseup", this.handleMouseUp);
+  React.useEffect(() => {
+    const sizeObserver = new ResizeObserver(entries => {
+      entries.forEach(({ target }) => {
+        if (
+          size.width !== target.clientWidth ||
+          size.height !== target.clientHeight
+        ) {
+          setSize({ width: target.clientWidth, height: target.clientHeight });
+        }
+      });
+    });
+    sizeObserver.observe(comRef.current);
+
+    return () => sizeObserver.disconnect();
+  }, [comRef]);
+
+  return size;
+};
+
+function usePosition({ menuWidth, menuHeight, isSelectingText, props }) {
+  const { view, active } = props;
+  const { selection } = view.state;
+
+  if (!active || !menuWidth || !menuHeight || SSR || isSelectingText) {
+    return defaultPosition;
   }
 
-  componentDidUpdate() {
-    const newState = this.calculatePosition(this.props);
+  // based on the start and end of the selection calculate the position at
+  // the center top
+  const fromPos = view.coordsAtPos(selection.$from.pos);
+  const toPos = view.coordsAtPos(selection.$to.pos);
 
-    if (!isEqual(newState, this.state)) {
-      this.setState(newState);
-    }
+  // ensure that start < end
+  const startPos = fromPos.left < toPos.left ? fromPos : toPos;
+  const endPos = fromPos.left < toPos.left ? toPos : fromPos;
+
+  // tables are an oddity, and need their own logic
+  const isColSelection = selection.isColSelection && selection.isColSelection();
+  const isRowSelection = selection.isRowSelection && selection.isRowSelection();
+
+  if (isRowSelection) {
+    endPos.left = startPos.left + 12;
+  } else if (isColSelection) {
+    const { node: element } = view.domAtPos(selection.$from.pos);
+    const { width } = element.getBoundingClientRect();
+    endPos.left = startPos.left + width;
   }
 
-  componentWillUnmount() {
-    window.removeEventListener("mousedown", this.handleMouseDown);
-    window.removeEventListener("mouseup", this.handleMouseUp);
-  }
+  const halfSelection = Math.abs(endPos.left - startPos.left) / 2;
+  const centerOfSelection = startPos.left + halfSelection;
 
-  handleMouseDown = () => {
-    if (!this.props.active) {
-      this.setState(state => ({ ...state, isSelectingText: true }));
-    }
+  // position the menu so that it is centered over the selection except in
+  // the cases where it would extend off the edge of the screen. In these
+  // instances leave a margin
+  const margin = 12;
+  const left = Math.min(
+    window.innerWidth - menuWidth - margin,
+    Math.max(margin, centerOfSelection - menuWidth / 2)
+  );
+  const top = Math.min(
+    window.innerHeight - menuHeight - margin,
+    Math.max(margin, Math.min(startPos.top, fromPos.top) - menuHeight)
+  );
+
+  // if the menu has been offset to not extend offscreen then we should adjust
+  // the position of the triangle underneath to correctly point to the center
+  // of the selection still
+  const offset = left - (centerOfSelection - menuWidth / 2);
+
+  return {
+    left: Math.round(left + window.scrollX),
+    top: Math.round(top + window.scrollY),
+    offset: Math.round(offset),
+    visible: true,
   };
+}
 
-  handleMouseUp = () => {
-    this.setState(state => ({ ...state, isSelectingText: false }));
-  };
+function FloatingToolbar(props) {
+  const menuRef = props.forwardedRef || React.createRef<HTMLDivElement>();
+  const [isSelectingText, setSelectingText] = React.useState(false);
+  const size = useComponentSize(menuRef);
+  const position = usePosition({
+    menuWidth: size.width,
+    menuHeight: size.height,
+    isSelectingText,
+    props,
+  });
 
-  calculatePosition(props) {
-    const { view, active } = props;
-    const { selection } = view.state;
-
-    if (!active || !this.menuRef.current || SSR || this.state.isSelectingText) {
-      return {
-        left: -1000,
-        top: 0,
-        offset: 0,
-        visible: false,
-        isSelectingText: this.state.isSelectingText,
-      };
-    }
-
-    // based on the start and end of the selection calculate the position at
-    // the center top
-    const startPos = view.coordsAtPos(selection.$from.pos);
-    const endPos = view.coordsAtPos(selection.$to.pos);
-
-    // tables are an oddity, and need their own logic
-    const isColSelection =
-      selection.isColSelection && selection.isColSelection();
-    const isRowSelection =
-      selection.isRowSelection && selection.isRowSelection();
-
-    if (isRowSelection) {
-      endPos.left = startPos.left + 12;
-    } else if (isColSelection) {
-      const { node: element } = view.domAtPos(selection.$from.pos);
-      const { width } = element.getBoundingClientRect();
-      endPos.left = startPos.left + width;
-    }
-
-    const halfSelection = Math.abs(endPos.left - startPos.left) / 2;
-    const centerOfSelection = startPos.left + halfSelection;
-
-    // position the menu so that it is centered over the selection except in
-    // the cases where it would extend off the edge of the screen. In these
-    // instances leave a margin
-    const { offsetWidth, offsetHeight } = this.menuRef.current;
-    const margin = 12;
-    const left = Math.min(
-      window.innerWidth - offsetWidth - margin,
-      Math.max(margin, centerOfSelection - offsetWidth / 2)
-    );
-    const top = Math.min(
-      window.innerHeight - offsetHeight - margin,
-      Math.max(margin, startPos.top - offsetHeight)
-    );
-
-    // if the menu has been offset to not extend offscreen then we should adjust
-    // the position of the triangle underneath to correctly point to the center
-    // of the selection still
-    const offset = Math.round(left - (centerOfSelection - offsetWidth / 2));
-
-    return {
-      left: Math.round(left + window.scrollX),
-      top: Math.round(top + window.scrollY),
-      offset,
-      visible: true,
-      isSelectingText: this.state.isSelectingText,
+  React.useEffect(() => {
+    const handleMouseDown = () => {
+      if (!props.active) {
+        setSelectingText(true);
+      }
     };
-  }
 
-  render() {
-    const { children, active } = this.props;
+    const handleMouseUp = () => {
+      setSelectingText(false);
+    };
 
-    // only render children when state is updated to visible
-    // to prevent gaining input focus before calculatePosition runs
-    return (
-      <Portal>
-        <Wrapper
-          active={active && this.state.visible}
-          ref={this.menuRef}
-          offset={this.state.offset}
-          style={{
-            top: `${this.state.top}px`,
-            left: `${this.state.left}px`,
-          }}
-        >
-          {this.state.visible && children}
-        </Wrapper>
-      </Portal>
-    );
-  }
+    window.addEventListener("mousedown", handleMouseDown);
+    window.addEventListener("mouseup", handleMouseUp);
+
+    return () => {
+      window.removeEventListener("mousedown", handleMouseDown);
+      window.removeEventListener("mouseup", handleMouseUp);
+    };
+  }, [props.active]);
+
+  // only render children when state is updated to visible
+  // to prevent gaining input focus before calculatePosition runs
+  return (
+    <Portal>
+      <Wrapper
+        active={props.active && position.visible}
+        ref={menuRef}
+        offset={position.offset}
+        style={{
+          top: `${position.top}px`,
+          left: `${position.left}px`,
+        }}
+      >
+        {position.visible && props.children}
+      </Wrapper>
+    </Portal>
+  );
 }
 
 const Wrapper = styled.div<{
@@ -190,8 +204,9 @@ const Wrapper = styled.div<{
   }
 `;
 
-export default React.forwardRef(
-  (props: Props, ref: React.RefObject<HTMLDivElement>) => (
-    <FloatingToolbar {...props} forwardedRef={ref} />
-  )
-);
+export default React.forwardRef(function FloatingToolbarWithForwardedRef(
+  props: Props,
+  ref: React.RefObject<HTMLDivElement>
+) {
+  return <FloatingToolbar {...props} forwardedRef={ref} />;
+});
