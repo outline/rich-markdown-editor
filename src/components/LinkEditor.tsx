20c0685f3f829dbe0f514d09faf36cae785a0629
Nan Yu <thenanyu@gmail.com>
2020-09-09 23:54:44 -0700

    remove unessecary bypass for URL inputs on link editor

--- src/components/LinkEditor.tsx
+++ src/components/LinkEditor.tsx
@@ -171,16 +171,13 @@ class LinkEditor extends React.Component<Props, State> {
 
   handleChange = async (event): Promise<void> => {
     const value = event.target.value;
-    const looksLikeUrl = isUrl(value);
 
     this.setState({
       value,
-      results: looksLikeUrl ? [] : this.state.results,
       selectedIndex: -1,
     });
 
-    // if it doesn't seem to be a url, try searching for matching documents
-    if (value && !looksLikeUrl && this.props.onSearchLink) {
+    if (value && this.props.onSearchLink) {
       try {
         const results = await this.props.onSearchLink(value);
         this.setState({ results });
