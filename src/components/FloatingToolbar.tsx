f1c6cfdbe0ec23951a2821d823db93579e6eb3c3
Rick Blalock <rblalock@pinpoint.com>
2020-09-11 17:26:44 -0400

    Add class to floating toolbar

--- src/components/FloatingToolbar.tsx
+++ src/components/FloatingToolbar.tsx
@@ -144,6 +144,7 @@ function FloatingToolbar(props) {
   return (
     <Portal>
       <Wrapper
+        className="floating-toolbar"
         active={props.active && position.visible}
         ref={menuRef}
         offset={position.offset}
