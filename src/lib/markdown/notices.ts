c9738c7b15472f6eb1d866ec729b61951c3befb6
Tom Moor <tom.moor@gmail.com>
2021-02-02 23:32:55 -0800

    fix: Add individual classnames to notices

--- src/lib/markdown/notices.ts
+++ src/lib/markdown/notices.ts
@@ -4,5 +4,16 @@ export default function notice(md): void {
   return customFence(md, "notice", {
     marker: ":",
     validate: () => true,
+    render: function(tokens, idx) {
+      const { info } = tokens[idx];
+
+      if (tokens[idx].nesting === 1) {
+        // opening tag
+        return `<div class="notice notice-${md.utils.escapeHtml(info)}">\n`;
+      } else {
+        // closing tag
+        return "</div>\n";
+      }
+    },
   });
 }
