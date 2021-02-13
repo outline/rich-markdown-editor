96e50285087dffcdd4d99f9a7941174fc7ccdc73
Tom Moor <tom.moor@gmail.com>
2021-02-13 10:58:39 -0800

    feat: Allow Shift-Enter to split list item (create newline)

--- src/nodes/ListItem.ts
+++ src/nodes/ListItem.ts
@@ -4,6 +4,7 @@ import {
   liftListItem,
 } from "prosemirror-schema-list";
 import Node from "./Node";
+import isInList from "../queries/isInList";
 
 export default class ListItem extends Node {
   get name() {
@@ -20,13 +21,21 @@ export default class ListItem extends Node {
     };
   }
 
-  keys({ type }) {
+  keys({ type, schema }) {
     return {
       Enter: splitListItem(type),
       Tab: sinkListItem(type),
       "Shift-Tab": liftListItem(type),
       "Mod-]": sinkListItem(type),
       "Mod-[": liftListItem(type),
+      "Shift-Enter": (state, dispatch) => {
+        if (!isInList(state)) return false;
+        if (!state.selection.empty) return false;
+
+        const { tr, selection } = state;
+        dispatch(tr.split(selection.to));
+        return true;
+      },
     };
   }
 
