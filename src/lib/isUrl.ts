81decf5ab56f4b0c8a4453b5528a364e663f6cbf
Dominic Zijlstra <dominiczijlstra@Dominics-Air.lan>
2021-03-11 15:13:23 +0100

    Improve url detection

--- src/lib/isUrl.ts
+++ src/lib/isUrl.ts
@@ -1,6 +1,9 @@
 export default function isUrl(text: string) {
   try {
     new URL(text);
+    if (!text.includes("://")) {
+      return false;
+    }
     return true;
   } catch (err) {
     return false;
