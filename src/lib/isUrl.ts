d02150e3919937cad7ca8c56dfc2c4360f0fd24d
Sergey Klinov <sergeyklinov@gmail.com>
2021-05-05 13:12:43 +0500

    Improve `isUrl()` by adding RegExp

--- src/lib/isUrl.ts
+++ src/lib/isUrl.ts
@@ -3,10 +3,7 @@ export default function isUrl(text: string) {
     return false;
   }
 
-  try {
-    new URL(text);
-    return true;
-  } catch (err) {
-    return false;
-  }
+  const urlRegExp: RegExp = /(mailto:[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$)|(((?:https?)|(?:ftp)):\/\/(?:www\.|(?!www))[a-zA-Z0-9][a-zA-Z0-9-]+[a-zA-Z0-9]\.[^\s]{2,}|www\.[a-zA-Z0-9][a-zA-Z0-9-]+[a-zA-Z0-9]\.[^\s]{2,}|https?:\/\/(?:www\.|(?!www))[a-zA-Z0-9]+\.[^\s]{2,}|www\.[a-zA-Z0-9]+\.[^\s]{2,})/;
+  
+  return urlRegExp.test(text)
 }
