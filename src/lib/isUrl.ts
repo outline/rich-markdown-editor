1b86e6af06890a0aa29304c321e5ffc60e5ff88e
Sergey Klinov <sergeyklinov@gmail.com>
2021-05-06 19:39:25 +0500

    Improve URL check not restricting protocols

--- src/lib/isUrl.ts
+++ src/lib/isUrl.ts
@@ -3,7 +3,10 @@ export default function isUrl(text: string) {
     return false;
   }
 
-  const urlRegExp: RegExp = /(mailto:[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$)|(((?:https?)|(?:ftp)):\/\/(?:www\.|(?!www))[a-zA-Z0-9][a-zA-Z0-9-]+[a-zA-Z0-9]\.[^\s]{2,}|www\.[a-zA-Z0-9][a-zA-Z0-9-]+[a-zA-Z0-9]\.[^\s]{2,}|https?:\/\/(?:www\.|(?!www))[a-zA-Z0-9]+\.[^\s]{2,}|www\.[a-zA-Z0-9]+\.[^\s]{2,})/;
-  
-  return urlRegExp.test(text)
+  try {
+    const url = new URL(text);
+    return url.hostname !== "";
+  } catch (err) {
+    return false;
+  }
 }
