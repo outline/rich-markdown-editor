e5f10d432f04e147169afde5de90069bcdac3eec
Tom Moor <tom.moor@gmail.com>
2021-05-21 21:54:07 -0700

    fix: Allow caret at end of inline code to toggle mark with arrow keys

--- src/queries/isInCode.ts
+++ src/queries/isInCode.ts
@@ -1,6 +1,7 @@
 import isMarkActive from "./isMarkActive";
+import { EditorState } from "prosemirror-state";
 
-export default function isInCode(state) {
+export default function isInCode(state: EditorState): boolean {
   const $head = state.selection.$head;
   for (let d = $head.depth; d > 0; d--) {
     if ($head.node(d).type === state.schema.nodes.code_block) {
