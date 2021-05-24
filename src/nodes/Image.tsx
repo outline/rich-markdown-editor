1842a406e18c0646369e3df5a7a43a25e4b2d64f
Tom Moor <tom.moor@gmail.com>
2021-05-23 18:50:19 -0700

    fix: Improve image download filename and interaction

--- src/nodes/Image.tsx
+++ src/nodes/Image.tsx
@@ -1,4 +1,5 @@
 import * as React from "react";
+import { DownloadIcon } from "outline-icons";
 import { Plugin, NodeSelection } from "prosemirror-state";
 import { InputRule } from "prosemirror-inputrules";
 import { setTextSelection } from "prosemirror-utils";
@@ -95,6 +96,24 @@ const getLayoutAndTitle = tokenTitle => {
       title: tokenTitle,
     };
   }
+};
+
+const downloadImageNode = async node => {
+  const image = await fetch(node.attrs.src);
+  const imageBlob = await image.blob();
+  const imageURL = URL.createObjectURL(imageBlob);
+  const extension = imageBlob.type.split("/")[1];
+  const potentialName = node.attrs.alt || "image";
+
+  // create a temporary link node and click it with our image data
+  const link = document.createElement("a");
+  link.href = imageURL;
+  link.download = `${potentialName}.${extension}`;
+  document.body.appendChild(link);
+  link.click();
+
+  // cleanup
+  document.body.removeChild(link);
 };
 
 export default class Image extends Node {
@@ -212,6 +231,12 @@ export default class Image extends Node {
     view.dispatch(transaction);
   };
 
+  handleDownload = ({ node }) => event => {
+    event.preventDefault();
+    event.stopPropagation();
+    downloadImageNode(node);
+  };
+
   component = props => {
     const { theme, isSelected } = props;
     const { alt, src, title, layoutClass } = props.node.attrs;
@@ -223,6 +248,12 @@ export default class Image extends Node {
           className={isSelected ? "ProseMirror-selectednode" : ""}
           onClick={this.handleSelect(props)}
         >
+          <Button>
+            <DownloadIcon
+              color="currentColor"
+              onClick={this.handleDownload(props)}
+            />
+          </Button>
           <ImageZoom
             image={{
               src,
@@ -289,22 +320,7 @@ export default class Image extends Node {
           return false;
         }
 
-        const image = await fetch(node.attrs.src);
-        const imageBlob = await image.blob();
-        const imageURL = URL.createObjectURL(imageBlob);
-
-        const parts = node.attrs.src.split("/");
-        const potentialName = parts[parts.length - 1];
-
-        // create a temporary link node and click it with our image data
-        const link = document.createElement("a");
-        link.href = imageURL;
-        link.download = potentialName || "download";
-        document.body.appendChild(link);
-        link.click();
-
-        // cleanup
-        document.body.removeChild(link);
+        downloadImageNode(node);
 
         return true;
       },
@@ -378,9 +394,43 @@ export default class Image extends Node {
   }
 }
 
+const Button = styled.button`
+  position: absolute;
+  top: 8px;
+  right: 8px;
+  border: 0;
+  margin: 0;
+  padding: 0;
+  border-radius: 4px;
+  background: ${props => props.theme.background};
+  color: ${props => props.theme.textSecondary};
+  width: 24px;
+  height: 24px;
+  display: inline-block;
+  cursor: pointer;
+  opacity: 0;
+  transition: opacity 100ms ease-in-out;
+
+  &:active {
+    transform: scale(0.98);
+  }
+
+  &:hover {
+    color: ${props => props.theme.text};
+    opacity: 1;
+  }
+`;
+
 const ImageWrapper = styled.span`
   line-height: 0;
   display: inline-block;
+  position: relative;
+
+  &:hover {
+    ${Button} {
+      opacity: 0.9;
+    }
+  }
 `;
 
 const Caption = styled.p`
