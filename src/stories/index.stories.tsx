9318bd4eb441915979a68c330af4e8328a0647ae
Tom Moor <tom.moor@gmail.com>
2021-07-27 15:41:15 -0400

    chore: Add readOnly control in storybook

--- src/stories/index.stories.tsx
+++ src/stories/index.stories.tsx
@@ -8,6 +8,7 @@ export default {
   title: "Editor",
   component: Editor,
   argTypes: {
+    readOnly: { control: "boolean" },
     onSave: { action: "save" },
     onCancel: { action: "cancel" },
     onClickHashtag: { action: "hashtag clicked" },
