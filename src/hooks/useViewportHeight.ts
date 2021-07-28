c0b10a592ee120757973f5a6f95040c3f3075119
Tom Moor <tom.moor@gmail.com>
2021-07-28 17:37:54 -0400

    note

--- src/hooks/useViewportHeight.ts
+++ src/hooks/useViewportHeight.ts
@@ -1,6 +1,9 @@
 import { useLayoutEffect, useState } from "react";
 
 export default function useViewportHeight(): number | void {
+  // https://developer.mozilla.org/en-US/docs/Web/API/VisualViewport#browser_compatibility
+  // Note: No support in Firefox at time of writing, however this mainly exists
+  // for virtual keyboards on mobile devices, so that's okay.
   const [height, setHeight] = useState<number>(
     () => window.visualViewport?.height || window.innerHeight
   );
