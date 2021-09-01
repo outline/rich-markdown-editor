576d7f85c73709495aead4d2c565d95d8b2b7e9a
Marvin <amazingmarvinapp@gmail.com>
2021-09-01 10:28:36 +0200

    fix: use current value in LinkEditor "Open link".

--- src/components/LinkEditor.tsx
+++ src/components/LinkEditor.tsx
@@ -218,7 +218,7 @@ class LinkEditor extends React.Component<Props, State> {
 
   handleOpenLink = (event): void => {
     event.preventDefault();
-    this.props.onClickLink(this.href, event);
+    this.props.onClickLink(this.state.value, event);
   };
 
   handleCreateLink = (value: string) => {
