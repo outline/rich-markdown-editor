1071d67a6f7dc6d31b857b9993eca2eda6083346
Tom Moor <tom.moor@gmail.com>
2021-11-29 21:36:48 -0600

    tsc

--- src/rules/emoji.ts
+++ src/rules/emoji.ts
@@ -1,7 +1,8 @@
 import nameToEmoji from "gemoji/name-to-emoji.json";
+import MarkdownIt from "markdown-it";
 import emojiPlugin from "markdown-it-emoji";
 
-export default function emoji(md): void {
+export default function emoji(md: MarkdownIt): (md: MarkdownIt) => void {
   return emojiPlugin(md, {
     defs: nameToEmoji,
     shortcuts: {},
